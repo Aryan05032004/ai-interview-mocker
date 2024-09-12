f2aa8882fd8959e4093a8267a4b56607cae42a66
Aryan05032004 <128672940+Aryan05032004@users.noreply.github.com>
2024-09-12 19:33:20 +0530

    changes

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+
+import DashboardLayout from '../dashboard/layout';
+
+const HomePage = () => {
+  return (
+    <DashboardLayout>
+      <div>Home Page Content</div>
+    </DashboardLayout>
+  );
+};
+
+export default HomePage;
